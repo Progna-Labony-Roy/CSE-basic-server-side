5b004772a71541f2f5b14db4f43b4d4ae80d16eb
Progna_Labony_Roy <labony.progna@gmail.com>
2022-10-25 22:10:06 +0600

    course categories file loaded

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app =express()
-const port =process.env.PORT || 5000
+const port =process.env.PORT || 5000;
+
+const categorires =require('./data/categories.json');
 
 app.get('/',(req, res) =>{
     res.send('News API');
+})
+
+app.get('/course-categories',(req,res) =>{
+    res.send(categorires)
 })
 
 app.listen(port,() =>{
